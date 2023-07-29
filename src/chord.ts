2b312b477411a1b28795491c947af9b2d35e0a14
Patrick Brady <patrick.brady24@outlook.com>
2023-07-29 11:01:49 -0500

    Recognize out of order notes

--- src/chord.ts
+++ src/chord.ts
@@ -2,10 +2,26 @@ import { BaseChord, InvertedChord, strategies } from './chords';
 import { Note } from './note';
 
 export abstract class Chord {
-  public static for(notes: string | Note[]): BaseChord | undefined {
-    const chordNotes = this.parse(notes);
+  /**
+   * Creates a chord from the notes
+   * @example
+   * // With a string
+   * Chord.for('C E G')?.getName(); // C
+   * 
+   * // With notes
+   * const C = Note.fromMidi(60);
+   * const E = Note.fromMidi(64);
+   * const G = Note.fromMidi(67);
+   * Chord.for([C, E, G])?.getName(); // C
+   */
+  public static for(input: string | Note[]): BaseChord | undefined {
+    const chordNotes = this.sanitizeInput(input);
 
     return this.getChord(chordNotes) || this.getInvertedChord(chordNotes);
+  }
+
+  private static sanitizeInput(notes: string | Note[]) {
+    return this.sortByMidiValue(this.parse(notes));
   }
 
   private static parse(notes: string | Note[]): Note[] {
@@ -14,6 +30,10 @@ export abstract class Chord {
     }
 
     return notes.split(' ').map((note) => new Note(note));
+  }
+
+  private static sortByMidiValue(notes: Note[]) {
+    return notes.sort((a, b) => a.getMidiValue() - b.getMidiValue());
   }
 
   private static getChord(notes: Note[]): BaseChord | undefined {
